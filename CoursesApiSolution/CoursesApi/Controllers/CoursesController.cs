a9d6a8ecc884d870aaadeaf328b958c17b604233
Jeffry Gonzalez <jeff@hypertheory.com>
2022-11-15 12:23:28 -0500

    updated docker compose

--- CoursesApiSolution/CoursesApi/Controllers/CoursesController.cs
+++ CoursesApiSolution/CoursesApi/Controllers/CoursesController.cs
@@ -2,8 +2,16 @@
 
 public class CoursesController : ControllerBase
 {
+
+    private readonly CoursesDataContext _context;
+
+    public CoursesController(CoursesDataContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet("/courses")]
-    public async  Task<ActionResult> GetCourses()
+    public async  Task<ActionResult> GetCoursesAsync()
     {
         return Ok();
     }
--- CoursesApiSolution/CoursesApi/Program.cs
+++ CoursesApiSolution/CoursesApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Adapters
+builder.Services.AddDbContext<CoursesDataContext>(config =>
+{
+    config.UseNpgsql(builder.Configuration.GetConnectionString("courses-db"));
+});
+
 
 var app = builder.Build();
 
